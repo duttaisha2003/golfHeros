43bf76198e419db9911007c45a7742d838f69e2b
Isha_Projects <duttaisha961@gmail.com>
2026-04-18 17:23:02 +0530

    cors updated

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: import.meta.env.VITE_API_URL|| 'http://localhost:5000',
+  withCredentials: true });
 
 // Attach token to every request
 API.interceptors.request.use((config) => {
